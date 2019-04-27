f7ad9e33536536104d93cdd8da08db13bb4629ea
anjiB72 <43617582+anjiB72@users.noreply.github.com>
2019-04-27 13:42:23 +0100

    add database callback to populate inventory

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Initialise Firebase
   var config = {
     apiKey: "AIzaSyASCsUTfCdp-9JUwWo06DxYgRpF-GJFeXI",
     authDomain: "simple-inventory-ab01.firebaseapp.com",
@@ -11,11 +11,61 @@
 
   var database = firebase.database();
 
+//function that hides the home page when the small container element is selected
+function hideHome(){
+	var element = document.getElementById('home');
+	element.classList.add('displayNone');
+};
+
+//function to retrieve products from database to display in table
+function getProducts(){
+	//calls the database just once and provides that instance of the results
+	database.ref('products').once('value', function(results){	
+		
+		//stores all the result values in a variable called allProducts
+		var allProducts = results.val();
+		
+		//set an empty array called inventory
+		var inventory = [];
+
+		//loops through all items in the allProducts variable
+		for (var item in allProducts){
+
+			var context = {
+		        productCategory: allProducts[item].productCategory,
+		        productID: allProducts[item].productId,
+		        productName: allProducts[item].productName,
+		        volumeSmall: allProducts[item].volumeSmall,
+		        volumeMedium: allProducts[item].volumeMedium,
+		        volumeLarge: allProducts[item].volumeLarge,
+		        volumeXlarge: allProducts[item].volumeXlarge,
+		        volumeSmallSock: allProducts[item].volumeSmallSock,
+		        volumeMediumSock: allProducts[item].volumeMediumSock,
+		        volumeSmallKid: allProducts[item].volumeSmallKid,
+		        volumeMediumKid: allProducts[item].volumeMediumKid,
+		        volumeLargeKid: allProducts[item].volumeLargeKid,
+		        productPrice: allProducts[item].productPrice,
+		        itemId: item
+	      	};
+
+	      var source = $("#entry-template").html();
+	      var template = Handlebars.compile(source);
+	      var inventoryItem = template(context);
+	      inventory.push(inventoryItem);
+	  	};
+
+    	// append each item to the inventory table in the DOM
+	    for (var i in inventory) {
+	      $('#inventory-table').append(inventory[i]);
+	    };
+	}); //End of database.ref('products') once results function
+}; //End of getProducts Function
+
 //jquery document ready
 $(document).ready(function(){
 
 //MAIN PAGE SELECTION
-	//
+	//Change background colour when mouse enters or leaves the small containers on screen
 	$('.small-container').on('mouseenter', function(){
 		$(this).css("background-color", "rgba(8, 103, 136, 1)");
 	});
@@ -27,18 +77,19 @@ $(document).ready(function(){
 	//select Add new product
 	$('#addNewProd').click(function(){
 	
-		//display form to add new product
+		//display the form to add new product and hide the main container
 		$('.main-container, #add').removeClass('displayNone');
-		$('#home').addClass('displayNone');
+		//calls hideHome function	
+		hideHome();
 	});
 	
-
 	//select update existing product
 	$('#updateProduct').click(function(){
 
 		//display form to update product
 		$('.main-container, #update').removeClass('displayNone');
-		$('#home').addClass('displayNone');
+		//calls hideHome function	
+		hideHome();
 	});
 
 	//select to view inventory
@@ -46,7 +97,10 @@ $(document).ready(function(){
 
 		//display form to update product
 		$('.main-container, #inventory-list').removeClass('displayNone');
-		$('#home').addClass('displayNone');
+		//calls getProduct function
+		getProducts();
+		//calls hideHome function	
+		hideHome();
 	});
 
 
@@ -57,7 +111,7 @@ $(document).ready(function(){
 		//set product category value to a variable
 		var productCat = $('#prodCat').val();
 
-		//if else conditions to display size inputs dependent on product category
+		//if else condition to display size inputs dependent on product category selected
 		if (productCat === "socks") {
 			$('#sockSizes').removeClass("displayNone");
 			$('#topSizes , #kidSizes').addClass('displayNone');
@@ -86,6 +140,11 @@ $(document).ready(function(){
 		var medium = $('#medium').val();
 		var large = $('#large').val();
 		var xLarge = $('#xLarge').val();
+		var smallSock = $('#smallSock').val();
+		var mediumSock = $('#mediumSock').val();
+		var smallKid = $('#smallKid').val();
+		var mediumKid = $('#mediumKid').val();
+		var largeKid = $('#largeKid').val();
 		var price = $('#price').val();
 
 		//clear down input, UI experience
@@ -96,6 +155,11 @@ $(document).ready(function(){
 		$('#medium').val('');
 		$('#large').val('');
 		$('#xLarge').val('');
+		$('#smallSock').val('');
+		$('#mediumSock').val('');
+		$('#smallKid').val('');
+		$('#mediumKid').val('');
+		$('#largeKid').val('');
 		$('#price').val('');
 
 		//create a section for products in database
@@ -111,9 +175,14 @@ $(document).ready(function(){
     		volumeMedium: medium,
     		volumeLarge: large,
     		volumeXlarge: xLarge,
+    		volumeSmallSock: smallSock,
+    		volumeMediumSock: mediumSock,
+    		volumeSmallKid: smallKid,
+    		volumeMediumKid: mediumKid,
+    		volumeLargeKid: largeKid,
     		productPrice: price
-  		});
-	});		
+  		}); // product details.push method end brackets
+	});	//add product submit function end brackets	
 
 
-});
+}); //jquery document ready end brackets
